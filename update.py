78a3f3c8e6fcb93f027f13a6f5c1f87d205262b9
Benjamin Kilpatrick <befk500@gmail.com>
2025-02-18 13:25:55 -0600

    Find files

--- update.py
+++ update.py
@@ -1,1 +1,12 @@
-print("spud spud")
+from pathlib import Path
+import sys
+import os
+
+root_dir = os.path.abspath(sys.argv[1]) if len(sys.argv) > 1 else  os.getcwd()
+print("looking in", root_dir)
+
+files = list(Path(root_dir).glob('**/*.py'))
+for path in files:
+    print(path)
+    # do ur stuff for each file here
+
